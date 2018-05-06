10a79df8d4a31319f2ad430fe0ba31388b7356cb
Julie PELISSOU <siclebree.fairway@hotmail.fr>
2018-05-06 19:11:27 +0200

    Avancement menu & listeJDRhome
    Avancement sur l'affichage des menus et affinement de la liste des JDRs de l'accueil

--- GM/src/app/default/app.component.ts
+++ GM/src/app/default/app.component.ts
@@ -8,9 +8,21 @@ import { Http } from '@angular/http'
 export class AppComponent implements OnInit {
   constructor(private _httpService: Http) { }
   apiValues: string[] = [];
+
+  private ShowMenu: boolean = false;
+  private ShowNotification: boolean = false;
+  
   ngOnInit() {
     this._httpService.get('/api/values').subscribe(values => {
       this.apiValues = values.json() as string[];
     });
+  }
+
+  private ShowAndHideMenu(): void {
+    this.ShowMenu = !this.ShowMenu;
+  }
+
+  private ShowAndHideNotif(): void {
+    this.ShowNotification = !this.ShowNotification;
   }
 }
